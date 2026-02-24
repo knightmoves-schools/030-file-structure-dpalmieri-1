0771e0eb9d14560c397b912ad7cdecd3dc7b530b
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-24 12:44:13 -0600

    Create Trainer.cs
    Created .cs file

---
+++ Trainer.cs
@@ -0,0 +1,24 @@
+namespace Trainer;
+
+public class Trainer{
+    public string Listen(Animal[] animals) {
+        string sounds = "";
+
+        foreach(Animal animal in animals){
+            if(typeof(Cat).IsInstanceOfType(animal))
+            {
+                sounds += ((Cat) animal).Say() + ", ";
+            } 
+            else if (typeof(Dog).IsInstanceOfType(animal))
+            {   
+                sounds += ((Dog) animal).Talk() + ", ";
+            } 
+            else if (typeof(Bird).IsInstanceOfType(animal))
+            {
+                sounds += ((Bird) animal).Sing() + ", ";
+            }
+        }
+
+        return sounds;
+    }
+}
